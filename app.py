66358a2b8bd66152a4a497498c3d4dbd6f704f56
Kiran160405 <kirannimmala2005@gmail.com>
2025-12-17 15:22:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 st.set_page_config(page_title="Student Task Dashboard", layout="wide")
 
-html_path = path("index.html").read_text(encoding='utf-8')
+html_path = path("index.html").read_text(encoding="utf-8")
 st.components.v1.html(html_file, height=800, scrolling=True)
+
 
